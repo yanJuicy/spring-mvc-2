6bb325b3ab0475df578b3a89658c7d8ae64463f3
yanJuicy <hanjub3@gmail.com>
2023-08-13 02:26:58 +0900

    주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -115,6 +115,12 @@ public class BasicController {
         return "basic/condition";
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
     @Data
     static class User {
         private String username;
